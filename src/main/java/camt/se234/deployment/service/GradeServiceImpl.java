77a3f46ec30295c333b0b0c646fdc10211a5b5dd
COLLAPse <4174tt@gmail.com>
2026-03-06 02:25:24 +0700

    Update logic of GradeServiceImpl

--- src/main/java/camt/se234/deployment/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/deployment/service/GradeServiceImpl.java
@@ -7,14 +7,14 @@ import org.springframework.stereotype.Service;
 public class GradeServiceImpl implements GradeService {
     @Override
     public String getGrade(double score) {
-        if (score > 79.5) {
+        if (score > 80) {
             return "A";
         }
-        else if (score > 74.5){
+        else if (score > 70){
             return "B";
-        }else if (score > 59.5){
+        }else if (score > 60){
             return "C";
-        }else if (score > 32.5) {
+        }else if (score > 50) {
             return "D";
         } else
             return "F";
